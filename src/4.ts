3535f52e83810efa24a9c1dff11e44cc799651e1
EvheniiDovhui <edovguy97@gmail.com>
2024-03-17 00:30:13 +0200

    hw 3 ts

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,73 @@
-const key = new Key();
+class Key {
+	private signature: number
 
-const house = new MyHouse(key);
-const person = new Person(key);
+	constructor() {
+		this.signature = Math.random()
+	}
 
-house.openDoor(person.getKey());
+	getSignature(): number {
+		return this.signature
+	}
+}
 
-house.comeIn(person);
+class Person {
+	private key: Key
 
+	constructor(key: Key) {
+		this.key = key
+	}
 
-export {};
+	getKey(): Key {
+		return this.key
+	}
+}
+
+abstract class House {
+	protected door: boolean
+	protected key: Key
+	protected tenants: Person[]
+
+	constructor(key: Key) {
+		this.door = false
+		this.key = key
+		this.tenants = []
+	}
+
+	abstract openDoor(key: Key): void
+
+	comeIn(person: Person): void {
+		if (this.door) {
+			this.tenants.push(person)
+			console.log(`${person.getKey().getSignature()} came into the house.`)
+		} else {
+			console.log(
+				`The door is closed. ${person.getKey().getSignature()} cannot come in.`
+			)
+		}
+	}
+}
+
+class MyHouse extends House {
+	constructor(key: Key) {
+		super(key)
+	}
+
+	openDoor(key: Key): void {
+		if (key.getSignature() === this.key.getSignature()) {
+			this.door = true
+			console.log('The door is open.')
+		} else {
+			console.log('Wrong key. The door remains closed.')
+		}
+	}
+}
+const key = new Key()
+
+const house = new MyHouse(key)
+const person = new Person(key)
+
+house.openDoor(person.getKey())
+
+house.comeIn(person)
+
+export {}
